c801d22b23742ae4c64a530f710d759c4c239327
hridayroy19 <hrhridoyroy503@gmail.com>
2024-05-01 13:12:36 +0600

    crat user tabil

--- src/component/pages/dashboard/adminDashboard/User.jsx
+++ src/component/pages/dashboard/adminDashboard/User.jsx
@@ -1,9 +1,65 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const User = () => {
+
+    const { refetch, data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+          const response = await fetch(`http://localhost:6001/user`);
+          return response.json();
+        },
+      });
+      
+      console.log(users);
+
+
     return (
         <div>
-            users
+            <div className='flex items-center justify-between m-4'>
+                <h1> All Users</h1>
+                <h1> Total Users:{users.length}</h1>
+            </div>
+            {/* tabil crate  */}
+            <div>
+            <div className="overflow-x-auto">
+  <table className="table table-zebra">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+      </tr>
+      {/* row 2 */}
+      <tr>
+        <th>2</th>
+        <td>Hart Hagerty</td>
+        <td>Desktop Support Technician</td>
+        <td>Purple</td>
+      </tr>
+      {/* row 3 */}
+      <tr>
+        <th>3</th>
+        <td>Brice Swyre</td>
+        <td>Tax Accountant</td>
+        <td>Red</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+            </div>
         </div>
     );
 };
--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,36 +1,90 @@
 import { FaBagShopping, FaUsers } from "react-icons/fa6";
 import { IoMdAddCircleOutline } from "react-icons/io";
-import { MdDashboard } from "react-icons/md";
-import { TiEdit } from "react-icons/ti";
+import { MdDashboard, MdDashboardCustomize } from "react-icons/md";
+import { TiEdit, TiUser } from "react-icons/ti";
 import { Link, Outlet } from "react-router-dom";
 
 const DashboardLayout = () => {
-    return (
-        <div>
-            <div className="drawer lg:drawer-open">
-  <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content flex flex-col items-center justify-center">
-    {/* Page content here */}
-    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-    <Outlet/>
-  
-  </div> 
-  <div className="drawer-side">
-    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
-    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-      {/* Sidebar content here */}
-      <li><Link to={'/dashboard'} ><MdDashboard /> Dashboard </Link ></li>
-      <li><Link to={'/dashboard/alluser'} > <FaUsers /> All Users</Link ></li>
-      <li><Link to={'/dashboard/managebooking'} > <FaBagShopping /> Manage Booking</Link ></li>     
-      <li><Link to={'/dashboard/addMenu'} > <IoMdAddCircleOutline /> Add Menu</Link ></li>     
-      <li><Link to={'/dashboard/manageItem'} > <TiEdit /> Manage Items</Link ></li>     
-      {/* <li><Link to={'dashboard/alluser'} >Manage Items</Link ></li>      */}
-    </ul>
-  
-  </div>
-</div>
+  return (
+    <div>
+      <div className="drawer sm:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col sm:items-start sm:justify-start my-2">
+          {/* Page content here */}
+
+          <div className=" flex items-center justify-between mx-4 mt-3">
+            <label
+              htmlFor="my-drawer-2"
+              className="btn btn-primary drawer-button lg:hidden"
+            >
+              <MdDashboardCustomize />
+            </label>
+            <button className="btn flex justify-center sm:hidden btn-primary rounded-full px-4 ">
+              <TiUser className="text-xl" />
+              logout
+            </button>
+          </div>
+
+         <div className=" mt-5 md:mt-2 mx-4">  
+         <Outlet />
+         </div>
         </div>
-    );
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-2"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            {/* Sidebar content here */}
+            <li>
+              <Link className="flex justify-start gap-10" to={"/dashboard"}>
+                <img
+                  src="https://i.ibb.co/2v9bdVg/food-logo-food-point-background-vector-image-644408-2018.jpg"
+                  className="w-10"
+                  alt=""
+                />
+                <span className="badge badge-secondary badge-outline">
+                  admin
+                </span>
+              </Link>
+            </li>
+            <hr className="mb-2 mt-1" />
+            <li>
+              <Link to={"/dashboard"}>
+                <MdDashboard /> Dashboard{" "}
+              </Link>
+            </li>
+            <li>
+              <Link to={"/dashboard/alluser"}>
+                {" "}
+                <FaUsers /> All Users
+              </Link>
+            </li>
+            <li>
+              <Link to={"/dashboard/managebooking"}>
+                {" "}
+                <FaBagShopping /> Manage Booking
+              </Link>
+            </li>
+            <li>
+              <Link to={"/dashboard/addMenu"}>
+                {" "}
+                <IoMdAddCircleOutline /> Add Menu
+              </Link>
+            </li>
+            <li>
+              <Link to={"/dashboard/manageItem"}>
+                {" "}
+                <TiEdit /> Manage Items
+              </Link>
+            </li>
+            {/* <li><Link to={'dashboard/alluser'} >Manage Items</Link ></li>      */}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
