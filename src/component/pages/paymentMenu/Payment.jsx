a246172a3aaf57f8ee997ba95bd971aa13e73e3c
hridayroy19 <hrhridoyroy503@gmail.com>
2024-05-10 18:32:04 +0600

    update stripe

--- src/component/pages/paymentMenu/Payment.jsx
+++ src/component/pages/paymentMenu/Payment.jsx
@@ -3,25 +3,25 @@ import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./CheckoutForm";
 import UseCart from "../../hooks/UseCart";
 
-
-
 const stripePromise = loadStripe(import.meta.env.VITE_Stripe_Pk);
 const Payment = () => {
-   const [cart] = UseCart();
-//    console.log(cart);
+  const [cart] = UseCart();
+  //    console.log(cart);
 
-const  cartTOtal = cart.reduce( (sum , item) => sum + item.price,0)
-// console.log(cartTOtal);
-const totalPrice = parseFloat(cartTOtal.toFixed(2));
-// console.log(totalPrice);
+  const cartTOtal = cart.reduce((sum, item) => sum + item.price, 0);
+  // console.log(cartTOtal);
+  const totalPrice = parseFloat(cartTOtal.toFixed(2));
+  // console.log(totalPrice);
 
-    return (
-        <div>
-     <Elements stripe={stripePromise}>
-      <CheckoutForm price={totalPrice} cart={cart} />
-    </Elements>
-        </div>
-    );
+  return (
+    <div>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm price={totalPrice} cart={cart} />
+      </Elements>
+    </div>
+  );
 };
 
 export default Payment;
+
+//  exam time not code push 
