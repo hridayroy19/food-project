b421542f0b15bdbc960e39e7e68150fbc4d182f9
hridayroy19 <hrhridoyroy503@gmail.com>
2024-05-22 18:39:20 +0600

    handel update sateus

--- src/component/pages/dashboard/adminDashboard/Managebooking.jsx
+++ src/component/pages/dashboard/adminDashboard/Managebooking.jsx
@@ -2,17 +2,36 @@ import React from "react";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import { useQuery } from "@tanstack/react-query";
 import { GiConfirmed } from "react-icons/gi";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import Swal from "sweetalert2";
+import useAxiosPrivet from "../../../hooks/useAxiosPrivet";
 
 const Managebooking = () => {
   const axiosPublic = useAxiosPublic();
-  const { data: orders = [],refetch,} = useQuery({
+  const axiosPrivet = useAxiosPrivet();
+  const { data: orders = [], refetch } = useQuery({
     queryKey: [" orders"],
     queryFn: async () => {
       const res = await axiosPublic.get("/payment/all");
       return res.data;
     },
   });
-console.log(orders);
+  console.log(orders);
+
+  const handelConfirmed = async (item) => {
+    console.log(item);
+    await axiosPrivet.patch(`/paypment/${item._id}`).then((res) => {
+      console.log(res);
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Your work has been saved",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      refetch();
+    });
+  };
 
   return (
     <div className=" w-full md:w-[870px] px-4 mx-auto">
@@ -31,7 +50,7 @@ console.log(orders);
                 <th>Price</th>
                 <th> Status </th>
                 <th>Confarm Order </th>
-                <th> Order Date </th>
+                <th> Delete </th>
               </tr>
             </thead>
             <tbody>
@@ -42,20 +61,26 @@ console.log(orders);
                   <th>{index + 1}</th>
                   <td>
                     <h1> {item.email} </h1>
-                    
                   </td>
                   <td> {item?.transitionId}</td>
-                  <td>
-                   {item.price}
-                  </td>
+                  <td>{item.price}</td>
+                  <th>{item.status}</th>
                   <th>
-                   {item.status}
+                    {item.status === "confirmed" ? (
+                      "Done"
+                    ) : (
+                      <button
+                        onClick={() => handelConfirmed(item)}
+                        className=" btn btn-xl ml-6 bg-orange-500"
+                      >
+                        <GiConfirmed />
+                      </button>
+                    )}
                   </th>
                   <th>
-                  <button className=" btn btn-xl ml-6 bg-orange-500"><GiConfirmed /></button>
-                  </th>
-                  <th>
-                   12/26/2025
+                    <button className=" btn btn-xl  bg-red">
+                      <RiDeleteBin6Line />
+                    </button>
                   </th>
                 </tr>
               ))}
