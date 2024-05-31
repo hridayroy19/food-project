bbcf28f745cecbec381307efe7e8a66e84a2f1be
hridayroy19 <hrhridoyroy503@gmail.com>
2024-05-31 21:20:23 +0600

    create a contect page and design

--- src/component/sheard/Navbar.jsx
+++ src/component/sheard/Navbar.jsx
@@ -1,30 +1,25 @@
 import React, { useContext, useEffect, useState } from "react";
-
 import Login from "../pages/Login";
 import { Authcontext } from "../provider/AuthProvider";
 import { Link } from "react-router-dom";
 import UseCart from "../hooks/UseCart";
+import { MdCall, MdEmail } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
 
 const Navbar = () => {
   const { user, logOut } = useContext(Authcontext);
-  // console.log(user?.email);
-
   const [cart, refetch] = UseCart();
-  // console.log(cart);
+  const [isSticky, setSticky] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
 
   const handelSignout = () => {
     logOut().then().catch();
   };
-  const [isSticky, setSticky] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       const offset = window.scrollY;
-      if (offset > 0) {
-        setSticky(true);
-      } else {
-        setSticky(false);
-      }
+      setSticky(offset > 0);
     };
     window.addEventListener("scroll", handleScroll);
 
@@ -35,47 +30,47 @@ const Navbar = () => {
 
   const navItems = (
     <>
-  <li>
-    <Link to="/" className="text-orange-500">
-      Home
-    </Link>
-  </li>
-  <li tabIndex={0}>
-    <details>
-      <summary>Menu</summary>
-      <ul className="p-2">
-        <li>
-          <Link to="/menu">All</Link>
-        </li>
-        <li>
-          <Link to="/menu/salad">Salad</Link>
-        </li>
-        <li>
-          <Link to="/menu/pizza">Pizza</Link>
-        </li>
-      </ul>
-    </details>
-  </li>
-  <li tabIndex={0}>
-    <details>
-      <summary>Services</summary>
-      <ul className="p-2">
-        <li>
-          <Link to="/services/online-order">Online Order</Link>
-        </li>
-        <li>
-          <Link to="/services/table-booking">Table Booking</Link>
-        </li>
-        <li>
-          <Link to="/services/order-tracking">Order Tracking</Link>
-        </li>
-      </ul>
-    </details>
-  </li>
-  <li>
-    <Link to="/offer">Offers</Link>
-  </li>
-</>
+      <li>
+        <Link to="/" className="text-orange-500">
+          Home
+        </Link>
+      </li>
+      <li tabIndex={0}>
+        <details>
+          <summary>Menu</summary>
+          <ul className="p-2">
+            <li>
+              <Link to="/menu">All</Link>
+            </li>
+            <li>
+              <Link to="/menu/salad">Salad</Link>
+            </li>
+            <li>
+              <Link to="/menu/pizza">Pizza</Link>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li tabIndex={0}>
+        <details>
+          <summary>Services</summary>
+          <ul className="p-2">
+            <li>
+              <Link to="/services/online-order">Online Order</Link>
+            </li>
+            <li>
+              <Link to="/services/table-booking">Table Booking</Link>
+            </li>
+            <li>
+              <Link to="/services/order-tracking">Order Tracking</Link>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <Link to="/offer">Offers</Link>
+      </li>
+    </>
   );
 
   return (
@@ -84,9 +79,7 @@ const Navbar = () => {
     >
       <div
         className={`navbar xl:px-24 ${
-          isSticky
-            ? "shadow-md bg-base-100 transition-all duration-300 ease-in-out"
-            : ""
+          isSticky ? "shadow-md bg-base-100 transition-all duration-300 ease-in-out" : ""
         }`}
       >
         <div className="navbar-start">
@@ -119,7 +112,6 @@ const Navbar = () => {
             href="/"
           >
             <p className="font-bold">
-              {" "}
               <span className="text-black">Flavor</span> Foods
             </p>
             <img
@@ -133,6 +125,56 @@ const Navbar = () => {
           <ul className="menu menu-horizontal px-1">{navItems}</ul>
         </div>
         <div className="navbar-end ">
+        <div className="drawer drawer-end">
+            <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              <label
+                htmlFor="my-drawer-4"
+                className=""
+              >
+                contact
+              </label>
+            </div>
+            <div className="drawer-side">
+              <label
+                htmlFor="my-drawer-4"
+                aria-label="close sidebar"
+                className="drawer-overlay"
+              ></label>
+              <div className="menu text-center p-4 w-80 py-24 min-h-full bg-white font-mono  text-base-content">
+                <h1 className=" text-center text-2xl text-orange-600 mb-7
+                "> NEW HOURS </h1>
+
+                <div>
+                <h4 className="text-xl"> MONDAY - TUESDAY</h4>
+                <p className="font-bold mt-1">12:00PM to  10:00PM</p>
+                </div>
+                <div className=" mt-6">
+                <h4  className="text-xl">  WEDNESDAY - SUNDAY</h4>
+                <p className="font-bold mt-1">12:00PM to  10:00PM</p>
+                </div>
+                <div className=" mt-6">
+                  <button className=" text-[24px] "> <MdCall /></button>
+                  <p className="font-bold mb-1 "> Call Now</p>
+                  <p> +088 01738211936</p>
+                  <p>+088 01789019093</p>
+                </div>
+                {/* location */}
+                <div className=" mt-6">
+                  <button className=" text-[24px] "> <FaLocationDot /></button>
+                  <p className="text-orange-600 mb-1 text-xl"> Dicrections Hero</p>
+                  <p> Dinajpur pti Mor</p>
+                </div>
+                {/* email code */}
+                <div className=" mt-6">
+                  <button className=" text-[24px] "> <MdEmail /></button>
+                  <p className="text-orange-600  mb-1 text-xl"> Message Here</p>
+                  <p className="text-[22px]"> hrhridoyroy503@gmail.com</p>
+                </div>
+             
+              </div>
+            </div>
+          </div>
           <button className="btn btn-ghost btn-circle hidden lg:flex">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -149,7 +191,7 @@ const Navbar = () => {
               />
             </svg>
           </button>
-          {/* card item added */}
+          
           <Link to={"/cartPage"}>
             <label
               tabIndex={0}
@@ -171,12 +213,36 @@ const Navbar = () => {
                   />
                 </svg>
                 <span className="badge badge-sm indicator-item">
-                  {" "}
-                  {cart?.length || 0}{" "}
+                  {cart?.length || 0}
                 </span>
               </div>
             </label>
           </Link>
+
+          {/* <div className="drawer drawer-end">
+            <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              <label
+                htmlFor="my-drawer-4"
+                className="drawer-button btn btn-primary"
+              >
+                Open drawer
+              </label>
+            </div>
+            <div className="drawer-side">
+              <label
+                htmlFor="my-drawer-4"
+                aria-label="close sidebar"
+                className="drawer-overlay"
+              ></label>
+              <ul className="menu p-4 w-80 py-24 min-h-full bg-base-200 text-base-content">
+                <li>
+                  <a>Sidebar Item 1 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio perferendis quos aliquam facilis. Nam perspiciatis rerum, odio veniam, pariatur ullam non corporis iste consequuntur odit a est velit officia culpa!</a>
+                </li>
+                <li><a>Sidebar Item 2</a></li>
+              </ul>
+            </div>
+          </div> */}
 
           {user ? (
             <div className="dropdown dropdown-end">
@@ -188,13 +254,13 @@ const Navbar = () => {
                 <div className="w-10 rounded-full">
                   {user.photoURL ? (
                     <img
-                      alt="Tailwind CSS Navbar component"
+                      alt="User avatar"
                       src={user?.photoURL}
                     />
                   ) : (
                     <img
                       src="https://i.ibb.co/4Y2KSb9/istockphoto-1300845620-612x612.jpg"
-                      alt=""
+                      alt="Default avatar"
                     />
                   )}
                 </div>
@@ -210,18 +276,11 @@ const Navbar = () => {
                   </Link>
                 </li>
                 <li>
-                  <a>
-                    {" "}
-                    <Link to={"/dashboard"}>Dashboard</Link>
-                  </a>
+                  <Link to={"/dashboard"}>Dashboard</Link>
                 </li>
                 <li>
-                  <a>
-                    {" "}
-                    <Link to={"/order"}>Order</Link>
-                  </a>
+                  <Link to={"/order"}>Order</Link>
                 </li>
-
                 <li>
                   <a>Settings</a>
                 </li>
@@ -233,14 +292,12 @@ const Navbar = () => {
           ) : (
             <>
               <button
-                onClick={() =>
-                  document.getElementById("my_modal_5").showModal()
-                }
-                className="bg-orange-500  px-6 py-2 rounded-full"
+                onClick={() => setShowLoginModal(true)}
+                className="bg-orange-500 px-6 py-2 rounded-full"
               >
                 Login
               </button>
-              <Login></Login>
+              {showLoginModal && <Login onClose={() => setShowLoginModal(false)} />}
             </>
           )}
         </div>
